f7446a4dd33fd120403c75f9224a4d4d4e505335
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-07-16 17:39:21 +0000

    Initial commit. Define first function

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+def random_chamber():
+    """
+    Randomizes a number from 1 to 6 which represents the 
+    6 chambers in the cylinder of the revolover and one
+    contains a bullet.
+    """
+    return random.randint(1, 6) != 3    #sets the third out of six chambers as the one with a bullet inside
